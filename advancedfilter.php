db8042b2cff0ec8ca34129990fc8eaf372efbb0d
Montashir Billah <79073862+montashir92@users.noreply.github.com>
2021-02-17 11:33:08 +0600

    Update advancedfilter.php

--- advancedfilter.php
+++ advancedfilter.php
@@ -1,50 +1,50 @@
 
 <?php
 //Advanced filter.
-			$intnum=300;
-			$min=1;
-			$max=200;
-			if(filter_var($intnum, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>$min, 
-			"max_range"=>$max))) === false){
-				echo "It is valide range";
-				
-			}
-			else
-			{
-				echo "It is not Valide range";
-			}
+	$intnum=300;
+	$min=1;
+	$max=200;
+	if(filter_var($intnum, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>$min, 
+	"max_range"=>$max))) === false){
+		echo "It is valide range";
+
+	}
+	else
+	{
+		echo "It is not Valide range";
+	}
 
 ?>
 <?php
-			$intnum=300;
-			$min=1;
-			$max=200;
-			if(filter_var($intnum, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>$min, 
-			"max_range"=>$max)))){
-				echo "It is valide range";
-				
-			}
-			else
-			{
-				echo "It is not Valide range";
-			}
+	$intnum=300;
+	$min=1;
+	$max=200;
+	if(filter_var($intnum, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>$min, 
+	"max_range"=>$max)))){
+		echo "It is valide range";
+
+	}
+	else
+	{
+		echo "It is not Valide range";
+	}
 
 
 ?>
 <?php
 //It is valide filter
-			$intnum=100;
-			$min=1;
-			$max=200;
-			if(filter_var($intnum, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>$min, 
-			"max_range"=>$max)))){
-				echo "It is valide range";
-				
-			}
-			else
-			{
-				echo "It is not Valide range";
-			}
+	$intnum=100;
+	$min=1;
+	$max=200;
+	if(filter_var($intnum, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>$min, 
+	"max_range"=>$max)))){
+		echo "It is valide range";
+
+	}
+	else
+	{
+		echo "It is not Valide range";
+	}
 
 ?>
 
@@ -53,26 +53,26 @@
 
 
 $url="http://www.trainingwithliveproject.com";
-			if(filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED)){
-				echo "this url is valide";
-			}
-			else
-			{
-				echo "it is not valide";
-			}
+	if(filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED)){
+		echo "this url is valide";
+	}
+	else
+	{
+		echo "it is not valide";
+	}
 ?>
 
 <?php
 //this query is valide.
 
 $url="http://www.trainingwithliveproject.com/index.html?q=123";
-			if(filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED)){
-				echo "this url is valide";
-			}
-			else
-			{
-				echo "it is not valide";
-			}
+	if(filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED)){
+		echo "this url is valide";
+	}
+	else
+	{
+		echo "it is not valide";
+	}
 
 
 
